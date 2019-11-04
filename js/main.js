ac4f3be86f5e731a09132e3be5b3cba6fdd37ba1
Nukkuu <maksderij1337@gmail.com>
2019-11-04 16:58:42 +0200

    first

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
+
+$('.owl-carousel').owlCarousel({
+	items:1,
+	lazyLoad:true,
+    loop:true,
+    margin:10,
+    autoHeight:true,
+})
